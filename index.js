93bb79b99aec725186e444faa8fdcc7e35cecef2
hoatq-fptaptech <hoatq4@fpt.edu.vn>
2021-07-20 17:53:31 +0700

    up code nodejs demo

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ app.listen(port,function (){
 });
 // cap quyen truy cap cac file static (css, img, jquery...)
 app.use(express.static("public"));
+
 // routing - bo dinh tuyen (nhan vien cua van phong)
 app.get("/",function (req,res){ // trang chu
     // res.send("<html><head><title</head></html>"); // html- chỉ là 1 chuỗi đối với máy chủ

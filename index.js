67ee0184d99d5ed1d445276b7854b77f584178d2
hoatq-fptaptech <hoatq4@fpt.edu.vn>
2021-08-19 19:58:13 +0700

    code query db with nodejs

--- index.js
+++ index.js
@@ -7,75 +7,55 @@ app.listen(port,function (){
 // cap quyen truy cap cac file static (css, img, jquery...)
 app.use(express.static("public"));
 app.set("view engine","ejs");// Báo rằng sẽ sử dụng ejs làm view engine
-var count = 0;// dem so luong nguoi vao web
-var products = [
-    {
-        id:1,
-        name:"Product 1",
-        image:"images/dtvn.jpg"
+// khai bao de ket noi database
+var mssql = require("mssql");
+// var config = { // thong tin may chu
+//     server: "DESKTOP-LDE3KDI",
+//     user:"sa",
+//     password:"sa123",
+//     database:"T2104E",
+//     stream: false,
+//     port:1433,
+//     options: {
+//         trustedConnection: true,
+//         encrypt: true,
+//         enableArithAbort: true,
+//         trustServerCertificate: true,
+//     },
+// }
+var config = { // thong tin may chu
+    server: "t2004e-waddbserver.database.windows.net",
+    user:"demosql",
+    password:"Abcd@123",
+    database:"WebApplication3_db",
+    stream: false,
+    port:1433,
+    options: {
+        trustedConnection: true,
+        encrypt: true,
+        enableArithAbort: true,
+        trustServerCertificate: true,
     },
-    {
-        id:2,
-        name:"Product 2",
-        image:"images/dtvn.jpg"
-    },
-    {
-        id:3,
-        name:"Product 3",
-        image:"images/dtvn.jpg"
-    },
-    {
-        id:4,
-        name:"Product 4",
-        image:"images/dtvn.jpg"
-    }
-];
+}
+// ket noi voi database
+mssql.connect(config,function (err){
+    if(err) console.log(err);
+    else console.log("connected database!");
+})
+// tao doi tuong truy van du lieu
+var sql = new mssql.Request();
 // routing - bo dinh tuyen (nhan vien cua van phong)
 app.get("/",function (req,res){ // trang chu
-    // res.send("<html><head><title</head></html>"); // html- chỉ là 1 chuỗi đối với máy chủ
-    // res.sendFile(__dirname+"/public/demobootstrap.html");  //__dirname -> xuất cho chúng ta 1 chuỗi đường dẫn chính xác trên máy tính đến thư mục này
-    count++;
-    res.render("home",{
-        count: count
-    });// tu dong hieu de lay file home.ejs trong thu muc views
+    res.send("Trang chu");
 });
+app.get("/khach-hang",function (req,res){
+    // can lay danh sach khach hang
+    var txt_sql = "select * from Ass5_KhachHang;";
+    sql.query(txt_sql,function (err,rows){
+        if(err) res.send(err);
+        else res.send(rows.recordset);
+    })
+});
+// liet ke danh sachs hang hoa
 
-app.get("/ke-toan",function (req,res){
-    // res.send("Xin chao, vui long nop tien..");
-   // res.sendFile(__dirname+"/public/assignment3.html");
-    count++;
-    res.render("ketoan",{
-        products:products
-    });
-});
-
-app.get("/chi-tiet",function (req,res){
-    var masp = req.query.id;
-    // khi co id, tim kiem theo id trong array
-    var p = {};
-    for(var i=0;i<products.length;i++){
-        if(products[i].id == masp){
-            p = products[i];
-            break;
-        }
-    }
-    res.render("chitiet",{
-        masp:masp,
-        p: p
-    });
-});
-// su dung tham so tinh
-app.get("/chi-tiet2/:id",function (req,res){
-    var masp = req.params.id;
-    var p = {};
-    for(var i=0;i<products.length;i++){
-        if(products[i].id == masp){
-            p = products[i];
-            break;
-        }
-    }
-    res.render("chitiet",{
-        masp:masp,
-        p: p
-    });
-})
+// liet ke danh sach don hang
